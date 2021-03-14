5aabd30ebb40729f98fe7a51591fe356de800a20
Gergo IVAN <ivgraai@gmail.com>
2021-03-14 16:04:36 +0000

    Test integration.

--- src/test/java/com/waracle/api/CakesApiControllerTests.java
+++ src/test/java/com/waracle/api/CakesApiControllerTests.java
@@ -1,7 +1,11 @@
 package com.waracle.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.waracle.model.Cake;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +14,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -33,6 +40,8 @@ public class CakesApiControllerTests {
     private TestRestTemplate restTemplate;
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
 
     @Test
     public void contextLoads() {
@@ -46,9 +55,21 @@ public class CakesApiControllerTests {
     }
 
     @Test
-    public void shouldReturnCakesJson() throws Exception {
+    public void shouldBeAccessibleNewCake() throws Exception {
+        Cake body = new Cake().title("Zserbo szelet").desc("Nepszeru es kedvelt magyar eredetu sutemeny").image("https://hu.wikipedia.org/wiki/Zserb%C3%B3#/media/F%C3%A1jl:ZserboSzelet.JPG");
         this.mockMvc.perform(get("/cakes").header("Accept", "application/json")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("title")));
+                .andExpect(content().string(not(containsString(body.getTitle()))));
+        this.mockMvc.perform(post("/cakes").contentType(MediaType.APPLICATION_JSON).content(asJsonString(body))).andExpect(status().isOk());
+        this.mockMvc.perform(get("/cakes").header("Accept", "application/json")).andDo(print())
+                .andExpect(jsonPath("$.[?(@.title == '" + body.getTitle() + "')]").exists());
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
