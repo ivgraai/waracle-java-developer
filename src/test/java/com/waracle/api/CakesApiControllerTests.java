7b9b91cc06924ac9cb29295b6b10fce63ef2266a
Gergo IVAN <ivgraai@gmail.com>
2021-03-14 10:13:31 +0000

    Test api.

--- src/test/java/com/waracle/api/CakesApiControllerTests.java
+++ src/test/java/com/waracle/api/CakesApiControllerTests.java
@@ -1,30 +1,38 @@
 package com.waracle.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author ivgraai
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class CakesApiControllerTests {
 
     @LocalServerPort
     private int port;
-
     @Autowired
     private CakesApiController controller;
-
     @Autowired
     private TestRestTemplate restTemplate;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void contextLoads() {
@@ -33,7 +41,14 @@ public class CakesApiControllerTests {
 
     @Test
     public void shouldReturnSwaggerUi() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/swagger-ui.html", String.class)).contains("id=\"swagger-ui-container\"");
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/swagger-ui.html", String.class))
+                .contains("id=\"swagger-ui-container\"");
+    }
+
+    @Test
+    public void shouldReturnCakesJson() throws Exception {
+        this.mockMvc.perform(get("/cakes").header("Accept", "application/json")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("title")));
     }
 
 }
