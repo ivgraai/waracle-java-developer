37e3cd7d02569774867059ffc42ad5e7ba65ad54
Gergo IVAN <ivgraai@gmail.com>
2021-03-14 08:14:57 +0000

    Sanity check.

---
+++ src/test/java/com/waracle/api/CakesApiControllerTests.java
@@ -0,0 +1,17 @@
+package com.waracle.api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author ivgraai
+ */
+@SpringBootTest
+public class CakesApiControllerTests {
+
+    @Test
+    public void contextLoads() {
+        // emty block
+    }
+
+}
