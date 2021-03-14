306a0886caf556d8db98ff44c50d6c8290b2ffb6
Gergo IVAN <ivgraai@gmail.com>
2021-03-14 09:23:19 +0000

    Test server starting and swagger ui.

--- src/test/java/com/waracle/api/CakesApiControllerTests.java
+++ src/test/java/com/waracle/api/CakesApiControllerTests.java
@@ -1,17 +1,39 @@
 package com.waracle.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author ivgraai
  */
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CakesApiControllerTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private CakesApiController controller;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void contextLoads() {
-        // emty block
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    public void shouldReturnSwaggerUi() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/swagger-ui.html", String.class)).contains("id=\"swagger-ui-container\"");
     }
 
 }
